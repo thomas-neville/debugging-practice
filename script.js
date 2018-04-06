78b99a52609b57dcae16442213e26e35c1a4ca14
Thomas Neville <tnev@uw.edu>
2018-04-05 19:33:36 -0700

    js TODOs add courses functional and for loop, css style typos

--- script.js
+++ script.js
@@ -34,9 +34,14 @@ window.onload = function(){
 // clears content in form fields, prints courseList objects to the page.
 // BUGFIX: This function isn't working properly- nothing gets output to the list on form submit
 function addACourse(){
+  event.preventDefault()
   var grade = parseFloat(this.elements["grade"].value);
+  console.log("grade:", grade)
   /*
     TODO: validate that "grade" value is a number between 1.0 and 4.0, stop processing if it is not.
+    
+    = /^[1-4]+$/
+    if()
 
     Checking 'grade typeof "number"' will always return true because we called parseFloat.
     We must instead check that it's value is not NaN.
@@ -51,6 +56,7 @@ function addACourse(){
   }
 
   courseList.push(newCourse)
+  console.log("courseList:", courseList)
 
   clearFormFields()
   outputList()
@@ -77,6 +83,7 @@ function clearGPA(){
 // Clears content in form fields.
 function clearFormFields(){
   // TODO: implement this function.
+  // document.getElementsByTagName("form").reset();
 }
 
 // Clear out list of courses and all content shown on the page
@@ -89,6 +96,18 @@ function clearData(){
 // Prints courseList objects to the page in a readable way.
 function outputList(){
   var list = document.getElementById("course-list");
+  list.innerHTML = ""
+  
+  for(var i = 0; i< courseList.length; i++){
+    console.log("name: ", courseList[i].name, "grade: ", courseList[i].grade)
+    
+    var newLi = document.createElement("li")
+    newLi.innerHTML = "name: " + courseList[i].name + " grade: " + courseList[i].grade
+    
+    list.appendChild(newLi)
+  }
+  
+    
   /*
     TODO: Clear the existing contents of the "list" element. Then, for each object in courseList,
     create an li element that holds the course's name and grade, and append
